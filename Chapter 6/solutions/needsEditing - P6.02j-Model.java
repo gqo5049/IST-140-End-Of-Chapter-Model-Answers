66d4f9bf932a3b6440ae931216bbdf4c174249bf
gqo5049 <92865953+gqo5049@users.noreply.github.com>
2022-10-30 23:23:45 -0400

    P6.02j-Model.java
    Fixed

--- Chapter 6/solutions/needsEditing - P6.02j-Model.java
+++ Chapter 6/solutions/needsEditing - P6.02j-Model.java
@@ -1,26 +1,21 @@
 public class Main
 {
-    public static boolean containsDuplicates(int[] arr) {
-        // Assume no dupes.
-        boolean dupes = false;
-
-        // Loop through array, checking for duplicates
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] == arr[j]) {
-                    dupes = true;
-                }
-            }
-        }
-        return dupes;
-    }
-
     public static void main(String[] args) {
         int[] arr1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 42};
         int[] arr2 = {2, 1, 3, 4, 5, 4, 7, 4, 9, 4};
 
         // Check if array 1 has dupes.
-        if (containsDuplicates(arr1)) {
+        boolean dupes = false;
+
+        // Loop through array, checking for duplicates
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = i + 1; j < arr1.length; j++) {
+                if (arr1[i] == arr1[j]) {
+                    dupes = true;
+                }
+            }
+        }
+        if (dupes) {
             System.out.println("Array contains duplicates.");
         } else {
             System.out.println("Array does NOT contain duplicates.");
@@ -28,7 +23,17 @@ public class Main
         System.out.println("Expected: Array does NOT contain duplicates.");
 
         // Check if array 2 has dupes.
-        if (containsDuplicates(arr2)) {
+        dupes = false;
+
+        // Loop through array, checking for duplicates
+        for (int i = 0; i < arr2.length; i++) {
+            for (int j = i + 1; j < arr2.length; j++) {
+                if (arr2[i] == arr2[j]) {
+                    dupes = true;
+                }
+            }
+        }
+        if (dupes) {
             System.out.println("Array contains duplicates.");
         } else {
             System.out.println("Array does NOT contain duplicates.");
