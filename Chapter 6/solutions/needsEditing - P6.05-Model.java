d98f39e3738bda31b267679cd43e7553d6d8015d
gqo5049 <92865953+gqo5049@users.noreply.github.com>
2022-10-30 23:34:00 -0400

    P6.05-Model.java
    Fixed

--- Chapter 6/solutions/needsEditing - P6.05-Model.java
+++ Chapter 6/solutions/needsEditing - P6.05-Model.java
@@ -1,53 +1,47 @@
 public class Main
 {
-   public static int removeMin(double[] input, int currentSize)
-   {
-      // Find the minimum value first
-      double min = input[0];
-      int minPos = 0;
-   
-      for (int i = 1; i < currentSize; i++)
-      {
-         if (input[i] < min)
-         {
-            min = input[i];
-            minPos = i;
-         }
-      }
-   
-      // Now "eliminate" the minimum value
-      for (int j = minPos; j < currentSize - 1; j++)
-      {
-         input[j] = input[j + 1];
-      }
-   
-      // Decrement the currentSize
-      currentSize--;
-      return currentSize;
-   }
+    public static void main(String[] args)
+    {
+        int[] myArray = new int[10];
+        int mySize = 0;
+        for (int i = 0; i < 6; i++)
+        {
+            myArray[i] = (int) (Math.random() * 100) +1;
+            mySize++;
+        }
 
-   public static void main(String[] args)
-   {
-      int[] myArray = new int[10];
-      int mySize = 0;
-      for (int i = 0; i < 6; i++)
-      {
-         myArray[i] = (int) (Math.random() * 100) +1;
-         mySize++;
-      }
+        for (int i = 0; i < mySize; i++)
+        {
+            System.out.print(myArray[i] + " ");
+        }
+        System.out.println();
 
-      for (int i = 0; i < mySize; i++)
-      {
-         System.out.print(myArray[i] + " ");
-      }
-      System.out.println();
+        // Find the minimum value first
+        double min = myArray[0];
+        int minPos = 0;
 
-      removeMin(myArray, mySize);
-   
-      for (int i = 0; i < mySize; i++)
-      {
-         System.out.print("myArray[i] + " ");
-      }
-      System.out.println();
-   }
+        for (int i = 1; i < mySize; i++)
+        {
+            if (myArray[i] < min)
+            {
+                min = myArray[i];
+                minPos = i;
+            }
+        }
+
+        // Now "eliminate" the minimum value
+        for (int j = minPos; j < mySize - 1; j++)
+        {
+            myArray[j] = myArray[j + 1];
+        }
+
+        // Decrement the currentSize
+        mySize--;
+
+        for (int i = 0; i < mySize; i++)
+        {
+            System.out.print(myArray[i] + " ");
+        }
+        System.out.println();
+    }
 }
