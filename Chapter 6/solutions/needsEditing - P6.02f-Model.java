0b8b724a39a8dff3cc14d98d3642be7a82b15c97
gqo5049 <92865953+gqo5049@users.noreply.github.com>
2022-10-30 23:05:48 -0400

    P6.02f-Model.java
    Fixed

--- Chapter 6/solutions/needsEditing - P6.02f-Model.java
+++ Chapter 6/solutions/needsEditing - P6.02f-Model.java
@@ -1,53 +1,46 @@
-public class Main
-{
-   public class MoveEvenToFront {
-      public static boolean moveEvenToFront(int[] arr) {
-         int endOfEvens = 0;
-         int temp;
-         boolean allEqual = true;
+public class Main {
+    public static void main(String[] args) {
+        int[] randoms = new int[10];
 
-         int value = arr[0];
-         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != value) {
-               allEqual = false;
-            }
-            if (arr[i] % 2 == 0) // Even
-            {
-               temp = arr[i];  // Save the even number
-
-               // Move array element from end of
-               // evens to current location
-               for (int j = i; j > endOfEvens; j--) {
-                  arr[j] = arr[j - 1];
-               }
-               arr[endOfEvens] = temp;
-               endOfEvens++;
-            }
-         }
-         return allEqual;
-      }
-
-      public static void main(String[] args) {
-         int[] randoms = new int[10];
-
-         // Create a test array containing random numbers.
-         for (int i = 0; i < randoms.length; i++) {
+        // Create a test array containing random numbers.
+        for (int i = 0; i < randoms.length; i++) {
             randoms[i] = (int) (Math.random() * 100) + 1;
             // Print the values as they are assigned.
             System.out.print(randoms[i] + " ");
-         }
-         System.out.println();
+        }
+        System.out.println();
 
-         // Move the evens to the front.
-         if (moveEvenToFront(randoms)) {
+        // Move the evens to the front.
+        int endOfEvens = 0;
+        int temp;
+        boolean allEqual = true;
+
+        int value = randoms[0];
+        for (int i = 0; i < randoms.length; i++) {
+            if (randoms[i] != value) {
+                allEqual = false;
+            }
+            if (randoms[i] % 2 == 0) // Even
+            {
+                temp = randoms[i];  // Save the even number
+
+                // Move array element from end of
+                // evens to current location
+                for (int j = i; j > endOfEvens; j--) {
+                    randoms[j] = randoms[j - 1];
+                }
+                randoms[endOfEvens] = temp;
+                endOfEvens++;
+            }
+        }
+        if (allEqual) {
             System.out.print("All elements are identical");
-         }
+        }
 
-         // Print again to see new order.
-         for (int i = 0; i < randoms.length; i++) {
+        // Print again to see new order.
+        for (int i = 0; i < randoms.length; i++) {
             System.out.print(randoms[i] + " ");
-         }
-         System.out.println();
-      }
-   }
+        }
+        System.out.println();
+    }
 }
