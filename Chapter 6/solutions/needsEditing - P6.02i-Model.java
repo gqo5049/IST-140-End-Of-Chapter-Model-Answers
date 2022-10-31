2ee24333a2e407c6584a6749b4895e45ca9793cb
gqo5049 <92865953+gqo5049@users.noreply.github.com>
2022-10-30 23:21:42 -0400

    P6.02i-Model.java
    Fixed

--- Chapter 6/solutions/needsEditing - P6.02i-Model.java
+++ Chapter 6/solutions/needsEditing - P6.02i-Model.java
@@ -1,25 +1,21 @@
 public class Main
 {
-    public static boolean adjacentDupes(int[] arr) {
-        // Assume no adjacent dupes.
-        boolean adjDupes = false;
-
-        // Loop through array, checking for duplicates
-        // next to each other.
-        for (int i = 0; i < arr.length - 1; i++) {
-            if (arr[i] == arr[i + 1]) {
-                adjDupes = true;
-            }
-        }
-        return adjDupes;
-    }
-
     public static void main(String[] args) {
         int[] arr1 = {1, 2, 3, 4, 4, 6, 7, 8, 9, 42};
         int[] arr2 = {2, 1, 3, 4, 5, 4, 7, 4, 9, 4};
 
         // Check if array 1 has adjacent dupes.
-        if (adjacentDupes(arr1)) {
+        // Assume no adjacent dupes.
+        boolean adjDupes = false;
+
+        // Loop through array, checking for duplicates
+        // next to each other.
+        for (int i = 0; i < arr1.length - 1; i++) {
+            if (arr1[i] == arr1[i + 1]) {
+                adjDupes = true;
+            }
+        }
+        if (adjDupes) {
             System.out.println("Array contains adjacent duplicates.");
         } else {
             System.out.println("Array does NOT contain adjacent duplicates.");
@@ -27,7 +23,17 @@ public class Main
         System.out.println("Expected: Array contains adjacent duplicates.");
 
         // Check if array 2 has adjacent dupes.
-        if (adjacentDupes(arr2)) {
+        // Assume no adjacent dupes.
+        adjDupes = false;
+
+        // Loop through array, checking for duplicates
+        // next to each other.
+        for (int i = 0; i < arr2.length - 1; i++) {
+            if (arr2[i] == arr2[i + 1]) {
+                adjDupes = true;
+            }
+        }
+        if (adjDupes) {
             System.out.println("Array contains adjacent duplicates.");
         } else {
             System.out.println("Array does NOT contain adjacent duplicates.");
